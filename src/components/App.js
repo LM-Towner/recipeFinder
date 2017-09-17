f7d04fe792f1e3b66711baa7dec26e3a7b33c2d5
La-Keisha Towner <ltowner622@gmail.com>
2017-09-17 16:51:08 -0400

    added Searchbar component into the App file

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-
+import SearchRecipes from "./SearchRecipes";
+import "../styles/index.css";
 export default class App extends Component {
   render() {
     return (
       <div>
         <h2>Find a recipe!</h2>
+        <SearchRecipes />
       </div>
     );
   }
