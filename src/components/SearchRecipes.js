893640465c3dbb67bff6c2c344349ef40de51104
La-Keisha Towner <ltowner622@gmail.com>
2017-09-17 18:16:31 -0400

    added api query strings, api is functioning as expected

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -8,24 +8,43 @@ import {
 } from "react-bootstrap";
 
 export default class SearchRecipes extends Component {
-    render() {
-        return (
-           <Form inline>
-            <FormGroup>
-              <ControlLabel>
-                Ingredients
-              </ControlLabel> 
-              {" "}
-              <FormControl type="text" placeholder="Chicken, Salsa" />
-            </FormGroup> 
-            <FormGroup>
-              <ControlLabel>Dish</ControlLabel>
-              {" "}
-              <FormControl type="text" placeholder="Tacos"></FormControl>
-            </FormGroup>
-            {" "}
-            <Button>Submit</Button>
-          </Form>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      ingredients: "",
+      dish: ""
+    };
+  }
+  search() {
+    let { ingredients, dish } = this.state;
+
+    // let ingredients = this.state.ingredients;
+    // let dish = this.state.dish;
+
+    const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+    console.log('state', this.state,"url", url);
+  }
+  render() {
+    return (
+      <Form inline>
+        <FormGroup>
+          <ControlLabel>Ingredients</ControlLabel>{" "}
+          <FormControl 
+            type="text"
+            placeholder="Chicken, Salsa" 
+            onChange={event => this.setState({ingredients: event.target.value})}
+            />
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>Dish</ControlLabel>{" "}
+          <FormControl 
+            type="text"
+            placeholder="Tacos"
+            onChange={event => this.setState({ dish: event.target.value })} 
+            />
+        </FormGroup>{" "}
+        <Button onClick={() => this.search()}>Submit</Button>
+      </Form>
+    );
+  }
 }
