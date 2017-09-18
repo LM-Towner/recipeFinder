639c16c2a0891a90837d8fe94fd5214c4bc9441f
La-Keisha Towner <ltowner622@gmail.com>
2017-09-17 23:22:18 -0400

    favoriting a recipe

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import {connect } from 'react-redux'
+import { favoriteRecipes } from '../actions';
 
 class RecipeItem extends Component {
   render() {
     let { recipe } = this.props;
     return (
       <div className="recipe-item">
+        <div onClick={()=> this.props.favoriteRecipes(recipe)}>
+         <div className='star'> &#9734;</div>
+        </div>
         <a href={recipe.href}>
-          <h4 classname="recipe=text">{recipe.title}</h4>
+          <h4 className="recipe=text">{recipe.title}</h4>
         </a>
         <p>{recipe.ingredients}</p>
 
@@ -16,4 +21,4 @@ class RecipeItem extends Component {
   }
 }
 
-export default RecipeItem;
+export default connect(null, { favoriteRecipes})(RecipeItem);
