1de7bf034f55350ec7223cbd258444fe474779e2
La-Keisha Towner <ltowner622@gmail.com>
2017-09-17 23:51:15 -0400

    added ability to favorite recipes

---
+++ src/components/FavoriteRecipeList.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import RecipeItem from "./RecipeItem";
+
+class FavoriteRecipeList extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Favorite Recipes:</h4>
+        <div>
+          {
+          this.props.favoriteRecipes.map((recipe, key) => {
+            return(
+            <RecipeItem key={key} recipe={recipe}/>
+            )
+          })
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    favoriteRecipes: state.favoriteRecipes
+  };
+}
+export default connect(mapStateToProps, null)(FavoriteRecipeList);
